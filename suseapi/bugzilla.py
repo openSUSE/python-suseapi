b380c12c105125a8501bdd600bed703a87c6608d
Michal Čihař <michal@cihar.com>
2013-10-22 15:04:16 +0200

    Add copyright headers

--- suseapi/bugzilla.py
+++ suseapi/bugzilla.py
@@ -1,4 +1,22 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2013 Michal Čihař <mcihar@suse.cz>
+#
+# This file is part of python-suseapi <https://github.com/nijel/python-suseapi>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 '''
 Generic access to Novell Bugzilla.
 
