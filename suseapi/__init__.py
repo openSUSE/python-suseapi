528fd226522447cec693e66988c25919108aa085
Michal Čihař <mcihar@suse.cz>
2013-10-31 16:30:56 +0100

    0.3

--- suseapi/__init__.py
+++ suseapi/__init__.py
@@ -17,6 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__version__ = '0.2'
+__version__ = '0.3'
 
 USER_AGENT = 'suseapi/{0}'.format(__version__)
