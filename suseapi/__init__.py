01efd56cf04bcc7c5ef732b5326b30ea9dcb736a
Michal Čihař <michal@cihar.com>
2013-12-17 10:12:37 +0100

    Setting version to 0.7.

--- suseapi/__init__.py
+++ suseapi/__init__.py
@@ -17,6 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__version__ = '0.6'
+__version__ = '0.7'
 
 USER_AGENT = 'python-suseapi/{0}'.format(__version__)
