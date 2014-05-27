298a72befd9cc0242ca4bc784fb4038c117c1108
Michal Čihař <michal@cihar.com>
2014-05-27 11:41:18 +0200

    Add testing for timeout
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- suseapi/test_browser.py
+++ suseapi/test_browser.py
@@ -23,9 +23,33 @@ Testing of browser wrapper.
 
 from unittest import TestCase
 import httpretty
+import time
+import threading
+import suseapi.browser
 from suseapi.browser import WebScraper, WebScraperError
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 TEST_BASE = 'http://example.net'
+
+
+class TimeoutHTTPHandler(BaseHTTPRequestHandler):
+    """
+    HTTP handler to emulate server timeouts.
+    """
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", 'text/html')
+        self.send_header('Connection', 'close')
+        self.end_headers()
+
+        print self.path
+
+        if self.path == '/foo':
+            self.wfile.write('<form method="GET" action="/bar" name="test">')
+            self.wfile.write('<input type="submit" value="Submit">')
+            self.wfile.write('</form>')
+        elif self.path == '/bar?':
+            time.sleep(1)
 
 
 class WebSraperTest(TestCase):
@@ -73,3 +97,24 @@ class WebSraperTest(TestCase):
         cookies = scraper.get_cookies()
         scraper.set_cookies(cookies)
         self.assertEquals(len(cookies), 0)
+
+    def test_timeout(self):
+        '''
+        Test timeout handling for stale requests.
+        '''
+        original_timeout = suseapi.browser.DEFAULT_TIMEOUT
+        suseapi.browser.DEFAULT_TIMEOUT = 0.5
+        server = HTTPServer(('localhost', 8888), TimeoutHTTPHandler)
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = False
+        server_thread.start()
+        try:
+            scraper = WebScraper(None, None, 'http://localhost:8888')
+            scraper.request('foo')
+            scraper.browser.select_form(nr=0)
+            self.assertRaises(WebScraperError, scraper._submit)
+            self.assertRaises(WebScraperError, scraper.request, 'bar?')
+        finally:
+            suseapi.browser.DEFAULT_TIMEOUT = original_timeout
+            server.shutdown()
+            server_thread.join()
