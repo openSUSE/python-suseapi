a49777dd4b16ef578eca3e3ab563d134024885c0
Michal Čihař <michal@cihar.com>
2014-07-22 14:14:42 +0200

    Test for cacher
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ suseapi/django_test_settings.py
@@ -0,0 +1,7 @@
+"""
+Settings for testing Django integration.
+"""
+BUGZILLA_USERNAME = 'test'
+BUGZILLA_PASSWORD = 'test'
+EMAIL_SUBJECT_PREFIX = '[TEST]'
+SECRET_KEY = 'test'
---
+++ suseapi/test_cacher.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2014 Michal Čihař <mcihar@suse.cz>
+#
+# This file is part of python-suseapi <https://github.com/nijel/python-suseapi>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+'''
+Testing of cacher mixin
+'''
+
+from unittest import TestCase
+import os
+
+from suseapi.cacher import CacherMixin, DjangoCacherMixin
+
+
+class CacherTest(TestCase):
+    cache = None
+
+    def setUp(self):
+        self.cache = CacherMixin()
+
+    def test_empty(self):
+        self.assertIsNone(self.cache.cache_get('empty'))
+
+    def test_set(self):
+        self.cache.cache_set('value', 42)
+        self.assertEqual(self.cache.cache_get('value'), 42)
+
+
+class DjangoCacherTest(CacherTest):
+    def setUp(self):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'suseapi.django_test_settings'
+        self.cache = DjangoCacherMixin()
