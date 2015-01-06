b48a0772679775f97d369dd258ba2da25e2e7026
Michal Čihař <michal@cihar.com>
2015-01-06 16:01:51 +0100

    Test presence command
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- suseapi/test_main.py
+++ suseapi/test_main.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from argparse import Namespace
+from StringIO import StringIO
 
-from suseapi.main import get_parser
+from suseapi.main import get_parser, main
+from suseapi.test_presence import start_test_server, stop_test_server
 
 
 class TestArgParser(TestCase):
@@ -13,3 +15,19 @@ class TestArgParser(TestCase):
             by='smart-uid',
             value=['foo']
         ))
+
+
+class TestCommands(TestCase):
+    def test_absence(self):
+        output = StringIO()
+        server = start_test_server()
+        try:
+            main(
+                settings=(('presence', 'servers', '127.0.0.1/nosend'),),
+                args=['absence', 'mcihar'],
+                stdout=output
+            )
+        finally:
+            stop_test_server(*server)
+
+        self.assertTrue('2013-10-25 - 2013-10-28' in output.getvalue())
